6b1e841c92655bc21913463776d367c1196358bd
myriem-moulouel <60098131+myriem-moulouel@users.noreply.github.com>
2023-01-15 17:14:08 +0100

    Update and rename game.py to game_synchro.py

--- Ressources/game_synchro.py
+++ Ressources/game_synchro.py
@@ -30,29 +30,13 @@ class Game:
                 for j in range(self.n_cols)
             ] for i in range(self.n_rows)
         ]
-        self.time_barrier = threading.Thread(target=self.refresh_barrier)
 
         # +1 car on attends aussi la time barrier
-        self.barrier_nei = threading.Barrier((n_rows * n_cols))
-        self.barrier_cell = threading.Barrier((n_rows * n_cols) + 1)
+        self.barrier = threading.Barrier((n_rows * n_cols))
 
     def quit(self):
         self.stop_event.set()
-        self.barrier_nei.abort()
-        self.barrier_cell.abort()
-        for sub_list in self.list_thread:
-            for thread in sub_list:
-                thread.join()
-
-    # Bloque pendant self.refresh_rate
-    # pour que les frames ne s'enchainent pas trop vite
-    def refresh_barrier(self):
-        while not self.stop_event.is_set():
-            time.sleep(self.refresh_rate)
-            try:
-                self.barrier_cell.wait()
-            except threading.BrokenBarrierError:
-                return
+        self.barrier.abort()
 
     # Indexation des cellules de la grille
     def create_cell(self, cell_type):
@@ -77,45 +61,35 @@ class Game:
         self.display_surface.fill(White)
 
         for row in range(self.n_rows):
-            pygame.draw.line(self.display_surface, Black, (0, row * self.row),
-                             (self.w, row * self.row))
+            pygame.draw.line(self.display_surface, Black, (0, row * self.row), (self.w, row * self.row))
 
         for col in range(self.n_cols):
-            pygame.draw.line(self.display_surface, Black, (col * self.col, 0),
-                             (col * self.col, self.h))
+            pygame.draw.line(self.display_surface, Black, (col * self.col, 0), (col * self.col, self.h))
 
         for row in range(len(self.grid_cells)):
             for col in range(len(self.grid_cells[row])):
                 if self.grid_cells[row][col]:
-                    Rect = pygame.Rect(col * self.col, row * self.row,
-                                       self.col, self.row)
+                    Rect = pygame.Rect(col * self.col, row * self.row, self.col, self.row)
                     pygame.draw.rect(self.display_surface, Black, Rect)
-
-    # Mettre une cellule vivante dans la case (i, j)
-    def draw_pattern(self, i, j):
-        self.grid_cells[i][j] = 1
 
     # mettre à jour la grille du nombre de voisins
     def update_grid(self):
         for r in range(self.n_rows):
             for c in range(self.n_cols):
                 self.list_thread[r][c].start()
-        self.time_barrier.start()
 
     def count_cells(self, r, c):
         while not self.stop_event.is_set():
             nei = 0
             for i in range(r - 1, r + 2):
                 for j in range(c - 1, c + 2):
-                    if ((i == r and j == c) or i < 0 or j < 0 or
-                            i == len(self.grid_cells) or j == len(
-                                self.grid_cells[0])):
+                    if ((i == r and j == c) or i < 0 or j < 0 or i == len(self.grid_cells) or j == len(self.grid_cells[0])):
                         continue
                     if self.grid_cells[i][j]:
                         nei += 1
             self.grid_neighbors[r][c] = nei
             try:
-                self.barrier_nei.wait()
+                self.barrier.wait()
             except threading.BrokenBarrierError:
                 return
 
@@ -129,10 +103,11 @@ class Game:
                     self.grid_cells[r][c] = 1
                 else:
                     self.grid_cells[r][c] = 0
-            if self.barrier_cell.n_waiting == self.n_rows * self.n_cols:
+            if self.barrier.n_waiting == self.n_rows * self.n_cols-1:
+                time.sleep(self.refresh_rate)
                 self.draw_grid()
 
             try:
-                self.barrier_cell.wait()
+                self.barrier.wait()
             except threading.BrokenBarrierError:
                 return
